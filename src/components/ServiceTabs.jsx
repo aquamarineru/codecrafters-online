94bfa42546fd5087b0e11ee053274a537776d6ab
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-09-27 16:33:15 +0200

    animation services

--- src/components/ServiceTabs.jsx
+++ src/components/ServiceTabs.jsx
@@ -5,51 +5,78 @@ import BlockContent from '@sanity/block-content-to-react';
 import { clientConfig } from '../../lib/client';
 import { motion, useAnimation } from 'framer-motion';
 
-
 const serializers = {
     types: {
-      mainImage: (props) => (
-        <div className="">
-          <Image
-            src={props.node.asset.url}
-            alt={props.node.alt}
-            className="w-full h-auto"
-            width={1000}
-            height={800}
-          />
-        </div>
-      ),
+        mainImage: (props) => (
+            <div className="">
+                <Image
+                    src={props.node.asset.url}
+                    alt={props.node.alt}
+                    className="w-full h-auto"
+                    width={1000}
+                    height={800}
+                />
+            </div>
+        ),
     },
-  };
-  
+};
 
-export default function ServiceTabs ({ serviceTabsData, locale }) {
+export default function ServiceTabs({ serviceTabsData, locale }) {
+    const webControls = useAnimation();
+    const otherTabsControls = useAnimation();
+    const [scrollY, setScrollY] = useState(0);
 
+    const sortedServiceTabsData = useMemo(() => {
+        const sortOrder = ["Design", "web", "dev"];
+        return [...serviceTabsData].sort((a, b) => {
+            return sortOrder.indexOf(a.tabName) - sortOrder.indexOf(b.tabName);
+        });
+    }, [serviceTabsData]);
 
-  const sortOrder = ["Design", "web", "dev"];
-  const sortedServiceTabsData = serviceTabsData.sort((a, b) => {
-    return sortOrder.indexOf(a.tabName) - sortOrder.indexOf(b.tabName);
-  });
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        // Assuming the maximum translation is 225 pixels as per your example
+        const maxTranslation = 150;
+        const translationFactor = maxTranslation / window.innerHeight;  // Adjust this factor based on your needs
+        const webXValue = -scrollY * translationFactor;
+        const otherTabsXValue = scrollY * translationFactor;
+
+        webControls.start({ x: webXValue });
+        otherTabsControls.start({ x: otherTabsXValue });
+    }, [scrollY, webControls, otherTabsControls]);
 
     return (
-        <div className="flex flex-wrap pt-10 text-light gap-10">
-        {Array.isArray(sortedServiceTabsData) &&
+        <div className="flex flex-wrap pt-10 text-light">
+            {Array.isArray(sortedServiceTabsData) &&
                 sortedServiceTabsData.map((serviceTabItem) => {
                     const localizedButton = serviceTabItem.button?.find(item => item._key === locale)?.value;
-                    return(
+                    const controls = serviceTabItem.tabName === "web" ? webControls : otherTabsControls;
+                    return (
                         <div
-                        key={serviceTabItem._id} 
-                        className="flex justify-center items-center gap-10">
-                            { serviceTabItem.tabName === "web" ? (
+                            key={serviceTabItem._id}
+                            className="flex justify-center items-center">
+                            {serviceTabItem.tabName === "web" ? (
                                 <>
-                                    <div
-                                    className='hidden md:block md:text-6xl xl:text-9xl uppercase font-black text-blue-600 relative z-0 pt-8 text-gray/70'>
+                                    <motion.div
+                                        animate={controls}
+                                        className='hidden md:block md:text-6xl xl:text-9xl uppercase font-black text-blue-600 relative z-0 pt-8 text-gray/70'>
                                         {serviceTabItem.tabName}
-                                    </div>
+                                    </motion.div>
                                     {serviceTabItem.body && (
                                         <div className='md:w-[40%] font-light '>
                                             <BlockContent
-                                                blocks={serviceTabItem.body[locale]} 
+                                                blocks={serviceTabItem.body[locale]}
                                                 serializers={serializers}
                                                 projectId={clientConfig.projectId}
                                                 dataset={clientConfig.dataset}
@@ -57,14 +84,13 @@ export default function ServiceTabs ({ serviceTabsData, locale }) {
                                             <button>{localizedButton} </button>
                                         </div>
                                     )}
-                                    
                                 </>
                             ) : (
                                 <>
                                     {serviceTabItem.body && (
                                         <div className='md:w-[40%] font-light '>
                                             <BlockContent
-                                                blocks={serviceTabItem.body[locale]} 
+                                                blocks={serviceTabItem.body[locale]}
                                                 serializers={serializers}
                                                 projectId={clientConfig.projectId}
                                                 dataset={clientConfig.dataset}
@@ -72,15 +98,17 @@ export default function ServiceTabs ({ serviceTabsData, locale }) {
                                             <button>{localizedButton} </button>
                                         </div>
                                     )}
-                                    <div 
-                                    
-                                    className='hidden md:block md:text-6xl xl:text-9xl uppercase font-black text-blue-600 relative z-0 pt-8 text-gray/70'>
+                                    <motion.div
+                                        animate={controls}
+                                        className='hidden md:block md:text-6xl xl:text-9xl uppercase font-black text-blue-600 relative z-0 pt-8 text-gray/70'>
                                         {serviceTabItem.tabName}
-                                    </div>  
+                                    </motion.div>
                                 </>
                             )}
                         </div>
-                    )})}
+                    )
+                })
+            }
         </div>
     )
 }
