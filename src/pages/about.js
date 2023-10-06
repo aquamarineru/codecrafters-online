5ee4c06a3ff072be711ab514172697a4e0f9c15c
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-10-06 18:15:38 +0200

    about resp

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,30 +23,32 @@ function AboutPage({ aboutPageData, locale }) {
                                 {localizedBtn}
                             </button>
                         </Link>
-                        <div className="flex items-center justify-between ">
+                        <div className="flex items-center justify-between gap-5">
                             <div className="flex flex-col pt-5 gap-10 md:w-[50%] ">
-                                <h1 className="uppercase font-h1 md:text-7xl  font-black  text-gray/70" >{localizedTitle} </h1>
-                                <p className="text-xl font-light" > {localizedDescription} </p>
+                                <h1 className="uppercase font-h1 text-3xl md:text-5xl  font-black  text-gray/70" >{localizedTitle} </h1>
+                                <p className="text-base md:text- font-light" > {localizedDescription} </p>
                             </div>
                             <Image
                             key={aboutPageItem._id}
                             src={urlFor(aboutPageItem.image).url()}
                             width={500}
                             height={500}
-                            className="rounded shadow-md"
+                            className="hidden md:block rounded shadow-md md:w-[50%]"
                             alt="about image"
                             />
 
                         </div>
-                        <div className="mt-24 h-full from-white-5 to-white-10 rounded border-[1px] border-light border-opacity-20 px-8 py-10 bg-opacity-40 bg-glass">
+                        <div className="mt-24 h-full from-white-5 to-white-10 rounded border-[1px] border-light border-opacity-20 px-8 py-10 bg-opacity-40 bg-glass flex flex-col justify-between gap-5">
                             {Array.isArray(aboutPageItem.mission) && aboutPageItem.mission.map((missionItem) => {
                                 const localizedTitle = missionItem.title.find(item => item._key === locale)?.value;
                                 const localizedSlogan = missionItem.slogan.find(item => item._key === locale)?.value;
                                 const localizedDescription = missionItem.description.find(item => item._key === locale)?.value;
                                 return(
-                                    <div key={missionItem._id}>
+                                    <div 
+                                    key={missionItem._id} 
+                                    >
                                         {localizedTitle === 'Our Vision' || localizedTitle === 'unsere Vision' ? (
-                                        <div className="flex flex-row items-center justify-around ">
+                                        <div className="flex flex-col gap-3 md:flex-row items-center justify-around ">
                                             <Image
                                                 key={missionItem._id}
                                                 src={urlFor(missionItem.image).url()}
@@ -56,18 +58,17 @@ function AboutPage({ aboutPageData, locale }) {
                                                 alt="vision image"
                                             />
                                             <div className="flex flex-col justify-between gap-3 md:w-[50%] ">
-                                                <h3 className="font-tag text-lg">{localizedTitle}</h3>
-                                                <h2 className="font-black font-h1 text-gray/80 text-4xl">{localizedSlogan}</h2>
-                                                <p className="text-lg font-light">{localizedDescription}</p>
+                                                <h3 className="font-tag text-sm md:text-base text-center md:text-start">{localizedTitle}</h3>
+                                                <h2 className="font-black font-h1 text-gray/80 text-xl md:text-2xl text-center md:text-start">{localizedSlogan}</h2>
+                                                <p className="text-base font-light text-center md:text-start">{localizedDescription}</p>
                                             </div>
                                         </div>
                                         ) : (
-                                        <div className="flex flex-row items-center justify-around ">
+                                        <div className="flex flex-col-reverse gap-3 md:flex-row items-center justify-around ">
                                             <div className="flex flex-col justify-between gap-3 md:w-[50%] ">
-                                                <h3 className="font-tag text-lg">{localizedTitle}</h3>
-                                                <h2 className="font-black font-h1 text-gray/80 text-4xl">{localizedSlogan}</h2>
-                                                <p className="text-lg font-light">{localizedDescription}</p>
-                                                    
+                                                <h3 className="font-tag text-sm md:text-base text-center md:text-start">{localizedTitle}</h3>
+                                                <h2 className="font-black text-center md:text-start font-h1 text-gray/80 text-xl md:text-2xl">{localizedSlogan}</h2>
+                                                <p className="text-center text-base  font-light md:text-start">{localizedDescription}</p>  
                                             </div>
                                                 <Image
                                                 key={missionItem._id}
@@ -89,15 +90,17 @@ function AboutPage({ aboutPageData, locale }) {
                             { Array.isArray(aboutPageItem.aboutBenefits) && aboutPageItem.aboutBenefits.map((aboutBenefitsItem) => {
                                 const localizedTitle = aboutBenefitsItem.title.find(item => item._key === locale)?.value;
                                 return(
-                                    <div key={aboutBenefitsItem._id} className="
-                                    flex flex-col items-center justify-center">
-                                        <h2 className="uppercase text-center font-h1 md:text-5xl  font-black  text-gray/70 w-[750px] "> {localizedTitle} </h2>
-                                        <div className="pt-24 flex flex-wrap items-start justify-center" >
+                                    <div 
+                                    key={aboutBenefitsItem._id} 
+                                    className="flex flex-col items-center justify-center">
+                                        <h2 className="uppercase text-center text-2xl font-h1 md:text-3xl md:w-[500px] font-black  text-gray/70 lg:w-[750px] "> {localizedTitle} </h2>
+                                        <div className="pt-16 md:pt-24 flex flex-col items-center gap-6 lg:flex-row md:items-start justify-center md:w-[400px] lg:w-full " >
                                         {aboutBenefitsItem.benefits && aboutBenefitsItem.benefits.map((benefit) => {
                                             const localizedBenefitTitle = benefit.title.find(item => item._key === locale)?.value;
                                             const localizedBenefitDescription = benefit.description.find(item => item._key === locale)?.value;
                                             return (
-                                                <div key={benefit.title} className="flex flex-col items-center m-4 w-1/4 from-white-5 to-white-10 rounded border-[1px] border-light border-opacity-20 px-4 py-8 bg-opacity-40 bg-glass relative">
+                                                <div 
+                                                key={benefit.title} className="flex flex-col items-center m-4 from-white-5 to-white-10 rounded border-[1px] border-light border-opacity-20 px-4 py-8 bg-opacity-40 bg-glass relative w-full ">
                                                      {benefit.image && <Image
                                                         src={urlFor(benefit.image).url()}
                                                         width={150}
