f8e7dad3b833d315f63a7e1cb4e263ce9feecd4c
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-10-14 14:04:59 +0200

    sitemap

---
+++ src/pages/api/sitemap.xml.js
@@ -0,0 +1,45 @@
+import { client } from "../../../lib/client";
+
+const baseUrl = 'https://www.codecrafters.online';
+const staticPages = [
+    '',
+    'about',
+    'impressum',
+    'services',
+    'projects',
+    'blog',
+  ];
+
+  async function fetchDynamicRoutes() {
+    const projectsQuery = '*[_type == "projectPage"]{slug}';
+    const blogPostsQuery = '*[_type == "post"]{slug}';
+    const projects = await client.fetch(projectsQuery);
+    const blogPosts = await client.fetch(blogPostsQuery);
+  
+    const projectRoutes = projects.map(project => `/projects/${encodeURIComponent(project.slug?.current || "")}`);
+    const blogRoutes = blogPosts.map(post => `blog/${encodeURIComponent(post.slug?.current || "")}`);
+  
+    return [...projectRoutes, ...blogRoutes];
+  }
+  const sitemapHandler = async (req, res) => {
+    const dynamicRoutes = await fetchDynamicRoutes();
+    const allRoutes = [...staticPages, ...dynamicRoutes];
+  
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${allRoutes.map(route => `
+        <url>
+          <loc>${baseUrl}/${route}</loc>
+          <changefreq>daily</changefreq>
+          <priority>0.7</priority>
+        </url>
+      `).join('')}
+    </urlset>
+  `;
+  
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(sitemap);
+    res.end();
+  };
+  
+  export default sitemapHandler;
