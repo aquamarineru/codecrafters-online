8c5213863be3cfba22814093c4e583fc6264526f
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-10-18 11:58:48 +0200

    pack

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,7 @@
-/* module.exports = {
+ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-} */
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
-      content: [
-        'src/**/*.html',
-        'src/**/*.js',
-        'src/**/*.jsx',
-        'src/styles/global.css',
-        // Or if using `src` directory structure
-        // './src/**/*.{js,jsx,ts,tsx}',
-        'public/**/*.html',
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    }),
-  ].filter(Boolean),
-};
+}
+
